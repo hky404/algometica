8211a1c25bd9691703771bb047b72d644e3fe5b1
hky404 <harshkumaryadavg@gmail.com>
2017-12-30 11:48:06 -0800

    did some code cleanup

--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
-import os, sys
+import os
+import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from flask.ext.script import Manager, Server
@@ -7,12 +8,12 @@ from algometica import app
 manager = Manager(app)
 
 manager.add_command("runserver", Server(
-	use_debugger = True,
-	use_reloader = True,
-	host = os.getenv('IP', '0.0.0.0'),
-	port = int(os.getenv('PORT', 5000))
-	)
+    use_debugger=True,
+    use_reloader=True,
+    host=os.getenv('IP', '0.0.0.0'),
+    port=int(os.getenv('PORT', 5000))
+)
 )
 
 if __name__ == "__main__":
-	manager.run()
+    manager.run()
