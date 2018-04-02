cf8660f5c71e3c74a7ab18523a6d477ac845918a
hky404 <harshkumaryadavg@gmail.com>
2018-04-01 20:01:31 -0700

    added author login functionality

--- author/views.py
+++ author/views.py
@@ -1,5 +1,39 @@
 from algometica import app
+from flask import render_template, redirect, url_for, session
+from author.form import RegisterForm, LoginForm
+from author.models import Author
 
-@app.route('/login')
+
+@app.route('/login', methods=('GET', 'POST'))
 def login():
-	return "hello, user!"
+    form = LoginForm()
+    error = None
+
+    if form.validate_on_submit():
+    	author = Author.query.filter_by(
+    		username = form.username.data,
+    		password = form.password.data
+    		).limit(1)
+    	if author.count():	# if author count > 0
+    		session['username'] = form.username.data
+    		return redirect(url_for('login_success'))
+
+    return render_template('author/login.html', form=form, error=error)
+
+
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        return redirect(url_for('success'))
+    return render_template('author/register.html', form=form)
+
+
+@app.route('/success')
+def success():
+    return 'Author registered!'
+
+@app.route('/login_success')
+def login_success():
+	return "Author logged in!"
